c9ddb33919f3d5b848a61fcf01eecd906845d033
Maria Luiza <marialluantunes@gmail.com>
2026-02-21 02:27:32 -0300

    Implementa views no app sistema_1 e no app sistema_2

--- sistema_1/views.py
+++ sistema_1/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import AlunoSistema1
+from .serializers import AlunoSistema1Serializer
+from rest_framework import viewsets, permissions
+from integracao.permissions import PermissaoBase, PermissaoProfessor
+
 
 # Create your views here.
+
+class AlunoSistema1ViewSet(viewsets.ModelViewSet):
+
+    queryset = AlunoSistema1.objects.all().order_by('nome')
+    serializer_class = AlunoSistema1Serializer
+    permission_classes = [PermissaoBase]
+
+    def get_view_name(self):
+        return "Cadastros no SUAP"
+
+
--- sistema_2/views.py
+++ sistema_2/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import AlunoSistema2
+from .serializers import AlunoSistma2Serializers
+from integracao.permissions import PermissaoBase, PermissaoProfessor
 
 # Create your views here.
+
+class AlunoMoodleViewSet(viewsets.ModelViewSet):
+    queryset = AlunoSistema2.objects.all().order_by('nome')
+    serializer_class = AlunoSistma2Serializers
+    permission_classes = [PermissaoProfessor]
+
+    def get_view_name(self):
+        return "Usuários do Moodle"
+    
+
